0cc067c227e6a1aa6f04acded00f4485d0c5375b
Vi Nguyen Tuong Vi <tuongvi@fossil.com>
2019-03-01 17:49:18 +0700

    Initial Commit

---
+++ HotKeywork/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HotKeywork
+//
+//  Created by Ms Vi Nguyen Tuong Vi on 3/1/19.
+//  Copyright © 2019 Ms Vi Nguyen Tuong Vi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
